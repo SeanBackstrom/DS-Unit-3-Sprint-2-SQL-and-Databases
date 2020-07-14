4163d0165c0fe0b687496bdaf864a590e96ac7cf
Sean Backstrom <sean.backstrom@gmail.com>
2020-07-14 08:29:51 -0700

    Add files via upload

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import sqlite3
 df = pd.read_csv("buddymove_holidayiq.csv")
-df.to_sql('review', con=sqlite3.Connection)
-conn = sqlite3.connect('review')
+    #Just run once
+#conn = df.to_sql('review', con=sqlite3.connect("buddymove_holidayiq.sqlite3"))
+conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 curs = conn.cursor()
-#I will finish this part 2 tomorrow
+
+print('Objective one:')
+count_rows = 'SELECT COUNT(*) FROM review;'
+print("count of rows:", curs.execute(count_rows).fetchall())
+
+print('Objective two:')
+count_rows = (f"SELECT COUNT(*) "
+                f"FROM review "
+                f"where Nature = (select Nature from review where Nature > 100) AND "
+                f"Shopping = (select Shopping from review where Shopping > 100) "
+            )
+print("count of >100 for Nature & Shopping:", curs.execute(count_rows).fetchall())
